540fa29e3241c172c5304dcca25b8445d58e1c5b
Benoit Gaschen <bens.57@hotmail.fr>
2015-11-26 15:56:18 +0100

    recup fichier de fonction

--- inc/functions.php
+++ inc/functions.php
@@ -1,14 +1,39 @@
-<?php
-	
-	/*
-	**
-	**	FICHIERS DE FONCTIONS
-	**
-	*/
+<?php 
+	//fonction qui insert le nom
+	function insertName($pdo, $firstname, $lastname){
+
+		$sql = 'INSERT INTO users (lastname, firstname) VALUES(:lastname, :firstname);';
+		$stmt = $pdo->prepare($sql);
+		$stmt->bindValue(':lastname', $lastname);
+		$stmt->bindValue(':firstname', $firstname);
+		$stmt->execute();
+	}
+	// fonction qui met à jour le nom 
+	function updateName($pdo, $firstname, $lastname){
+		$sql = 'UPDATE users SET lastname = :lastname, firstname = :firstname WHERE id = 1' ;
+		$stmt = $pdo->prepare($sql);
+		$stmt->bindValue(':lastname', $lastname);
+		$stmt->bindValue(':firstname', $firstname);
+		$stmt->execute();
+
+	}
+	//fonction qui récupere le nom 
+	function recupName($pdo) {
+		$sql = 'SELECT id, lastname, firstname FROM Users' ;
+		$stmt = $pdo->prepare($sql);
+		$stmt->execute();
+
+		$users = $stmt->fetch();
+		return $users;
+	}
+
 
 	// function qui renvoi le type de la page
 	function get_page(){
 		 return $_GET['page'];
 	}
 
-?>
+	// function qui récupère les images pour le slider en DBB
+	function get_image($pdo, $id){
+		
+	}
