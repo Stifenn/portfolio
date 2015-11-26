b42868f3f6aa259caa32e3ad84fb394a284dce8c
Stifenn <stifenn@gmail.com>
2015-11-26 15:41:20 +0100

    modif stifenn

---
+++ inc/functions.php
@@ -0,0 +1,35 @@
+
+<?php 
+	//fonction qui insert le nom
+	function insertName($pdo, $firstname, $lastname){
+
+		$sql = 'INSERT INTO users (lastname, firstname) VALUES(:lastname, :firstname);';
+		$stmt = $pdo->prepare($sql);
+		$stmt->bindValue(':lastname', $lastname);
+		$stmt->bindValue(':firstname', $firstname);
+		$stmt->execute();
+	}
+	// fonction qui met à jour le nom 
+	function updateName($pdo, $firstname, $lastname){
+		$sql = 'UPDATE users SET lastname = :lastname, firstname = :firstname WHERE id = 1' ;
+		$stmt = $pdo->prepare($sql);
+		$stmt->bindValue(':lastname', $lastname);
+		$stmt->bindValue(':firstname', $firstname);
+		$stmt->execute();
+
+	}
+	//fonction qui récupere le nom 
+	function recupName($pdo) {
+		$sql = 'SELECT id, lastname, firstname FROM Users' ;
+		$stmt = $pdo->prepare($sql);
+		$stmt->execute();
+
+		$users = $stmt->fetch();
+		return $users;
+	}
+
+
+	// function qui renvoi le type de la page
+	function get_page(){
+		 return $_GET['page'];
+	}
