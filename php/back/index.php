b76f2a81ec46a8ff82b790a407d5d7e04f0c25af
Stifenn <stifenn@gmail.com>
2015-11-26 12:01:52 +0100

    modif
    assignation des fichiers

---
+++ php/back/index.php
@@ -0,0 +1,29 @@
+<?php
+	
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Administration</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<main>
+		<nav>
+			<ul>
+				<li>Administration</li>
+				<li><a href="index.php" title="">Géneral</a></li>
+				<li><a href="" title="">Slider</a></li>
+				<li><a href="" title="">Contenu de la Home page</a></li>
+				<li><a href="" title="">Portfolio</a></li>
+			</ul>
+		</nav>
+		<section>
+			
+		</section>
+	</main>
+</body>
+</html>
